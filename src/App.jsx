a353a37832401cec25c78e3cb8fc4d4bbeb4456c
Dmitriy.Nikiforov <Nikiforov.DAl@gazpromneft-ntc.ru>
2023-10-10 19:55:02 +0300

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/about'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
 
 export default function App() {
   return (
-    <h1 className='text-red-700'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
