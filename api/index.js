1b4a9bd4979e876665be63ce741c6b8d1da8aab4
Dmitriy.Nikiforov <Nikiforov.DAl@gazpromneft-ntc.ru>
2023-10-10 23:34:13 +0300

    Set the connection to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO_DB)
+    .then(() => {
+        console.log('Connected to database');
+    }).catch((e) => {
+        console.log(e);
+    })
 
 const app = express()
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
+
